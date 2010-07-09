a46bee25e8b96a91a2eefca8530f21ccdcb03e54
Jimmy Forrester-Fellowes <jimmyxx@gmail.com>
2010-07-09 15:53:06 +0100

    added a minimum age validator.

---
+++ classes/validators/validate_MinAge.php
@@ -0,0 +1,28 @@
+<?php
+
+class validate_MinAge extends validate_AbstractValidator {
+
+	protected $maxAge;
+
+	public function __construct($minAge) {
+		$this->minAge = $minAge;
+	}
+
+	public function validate($data) {
+
+		if($this->ageDifference($data) < $this->minAge)
+			throw new Exception(sf('You must be older than %s years old', $this->minAge));
+	}
+
+	public function ageDifference($data) {
+		$yearDiff  = date('Y') - (int)$data['year'];
+		$monthDiff = date('n') - (int)$data['month'];
+		$dayDiff   = date('j') - (int)$data['day'];
+
+		if($dayDiff < 0 || $monthDiff < 0)
+			$yearDiff--;
+
+		return $yearDiff;
+	}
+}
+?>
