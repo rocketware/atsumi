5430f4a06617c68cc063ae3cc803fc2b80a94dde
Jimmy Forrester-Fellowes <jimmyxx@gmail.com>
2010-08-18 21:42:20 +0100

    added html5 placeholder attr

--- classes/widgets/form/elements/widget_TextElement.php
+++ classes/widgets/form/elements/widget_TextElement.php
@@ -4,19 +4,22 @@ class widget_TextElement extends widget_AbstractElement {
 
 	protected $htmlType 		= 'text';
 	protected $cssClassName 	= 'inputText';
+	protected $placeholder		= '';
 	protected $rows;
 
 	public function __construct($args) {
-
+		if (array_key_exists('placeholder', $args) && strlen($args['placeholder']))
+			$this->placeholder = $args['placeholder'];
 	}
 
 	function renderElement() {
-		return(sf('<input type="%s" name="%s" value="%s" id="form_%s" class="%s" />',
+		return(sf('<input type="%s" name="%s" value="%s" id="form_%s" class="%s"%s />',
 							$this->htmlType,
 							$this->getName(),
 							parent::makeInputSafe($this->getValue()),
 							$this->getName(),
-							$this->cssClassName
+							$this->cssClassName,
+							strlen($this->placeholder)?sf(' placeholder="%s"', $this->placeholder):''
 				));
 	}
 }
