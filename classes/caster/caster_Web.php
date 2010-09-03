131197e2c8be3fb02aa989dbfe33c483a482fe9c
Jimmy Forrester-Fellowes <jimmyxx@gmail.com>
2010-09-03 15:26:29 +0100

    disabled double_encode param in htmlspecialchars as causes errors on servers with < PHP 5.2.3

--- classes/caster/caster_Web.php
+++ classes/caster/caster_Web.php
@@ -2,7 +2,7 @@
 
 /* generic caster for an web project */
 class caster_Web extends caster_Abstract {
-	
+
 	protected $spec = array (
 		's' => 'string',
 		'd' => 'd',
@@ -12,24 +12,24 @@ class caster_Web extends caster_Abstract {
 		'u' => 'urlEncode',
 		'U' => 'urlDecode'
 		);
-		
+
 	/* annoyance due to PHP scope issue */
 	static function cast ($args) {
 		$parser =  new self();
 		return  (string) $parser->castString(func_get_args());
 	}
-		
+
 	static function string ($in) {
-		return $in;	
+		return $in;
 	}
 	static function d ($in) {
 		return sprintf('%d', $in);
 	}
 	static function html ($in) {
-		return htmlspecialchars($in, ENT_COMPAT, 'UTF-8', true);
+		return htmlspecialchars($in, ENT_COMPAT, 'UTF-8');
 	}
 	static function htmlLineBreaks ($in) {
-		return nl2br(htmlspecialchars($in, ENT_COMPAT, 'UTF-8', true));
+		return nl2br(htmlspecialchars($in, ENT_COMPAT, 'UTF-8'));
 	}
 	static function javascriptEscape ($in) {
 		return str_replace (
