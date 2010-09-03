9ea25eb71076d6157efc270747242ec2284e80c2
apparatusdeus <apparatusdeus@hotmail.co.uk>
2010-09-03 11:06:42 +0100

    Cleaned up the caster files and added comments.

--- classes/caster/exceptions/caster_Exception.php
+++ classes/caster/exceptions/caster_Exception.php
@@ -1,1 +1,20 @@
-<?php class caster_Exception extends Exception { } ?>
+<?php
+/**
+ * File defines all functionaility of the caster_Exception class.
+ * @package		Atsumi.Framework
+ * @copyright	Copyright(C) 2008, James A. Forrester-Fellowes. All rights reserved.
+ * @license		GNU/GPL, see license.txt
+ * The Atsumi Framework is open-source software. This version may have been modified pursuant to
+ * the GNU General Public License, and as distributed it includes or is derivative of works
+ * licensed under the GNU General Public License or other free or open source software licenses.
+ * See copyright.txt for copyright notices and details.
+ */
+
+/**
+ * Base exception class for all exceptions thrown by the caster classes
+ * @package		Atsumi.Framework
+ * @subpackage	Caster
+ * @since		1.0
+ */
+class caster_Exception extends Exception { }
+?>
