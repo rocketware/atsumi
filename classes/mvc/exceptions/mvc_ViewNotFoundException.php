0f578558a2c47562ca37463a803932951cd67e98
Jimmy Forrester-Fellowes <jimmyxx@gmail.com>
2010-09-06 15:37:14 +0100

    minor fix for bug if view is not found in text/plain context

--- classes/mvc/exceptions/mvc_ViewNotFoundException.php
+++ classes/mvc/exceptions/mvc_ViewNotFoundException.php
@@ -1,13 +1,13 @@
 <?php
-class mvc_ViewNotFoundException extends atsumi_AbstractException { 
-	
+class mvc_ViewNotFoundException extends atsumi_AbstractException {
+
 	private $viewName;
-	
+
 	public function __construct($message = null, $viewName, $code = 0) {
 		$this->viewName = $viewName;
 		parent::__construct($message, $code);
 	}
-	
+
 	public function getInstructions($contentType) {
 		switch($contentType) {
 			default:
@@ -19,10 +19,10 @@ class mvc_ViewNotFoundException extends atsumi_AbstractException {
 		public function renderBodyContent() {
 			/* ... put view code here ... */
 		}
-	}\n', $this->viewName);				
+	}\n', $this->viewName, $this->viewName);
 				break;
 			case 'text/html':
-						
+
 				return sf('<h4>Description</h4><p>The view class you specified could not be found. Please make sure you have named the class name the same as the filename and the class folder is loaded using Atsumi\'s load method.</p>
 <h4>Example basic HTML view</h4>
 <pre class="code">
@@ -34,11 +34,11 @@ class %s extends mvc_HtmlView {
 }
 </pre>', $this->viewName, $this->viewName);
 				break;
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 }
 ?>
