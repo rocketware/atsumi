b305164c36cd1dbd4e0ff97352ad40a3d3462221
Jimmy Forrester-Fellowes <jimmyxx@gmail.com>
2011-05-10 15:48:13 +0100

    added hooks for js onblur onfocus onkeydown onchange in the widget_TextElement

--- classes/widgets/form/elements/widget_TextElement.php
+++ classes/widgets/form/elements/widget_TextElement.php
@@ -5,21 +5,42 @@ class widget_TextElement extends widget_AbstractElement {
 	protected $htmlType 		= 'text';
 	protected $cssClassName 	= 'inputText';
 	protected $placeholder		= '';
+	protected $onChange			= null;
+	protected $onKeydown		= null;
+	protected $onFocus			= null;
+	protected $onBlur			= null;
 	protected $rows;
 
 	public function __construct($args) {
 		if (array_key_exists('placeholder', $args) && strlen($args['placeholder']))
 			$this->placeholder = $args['placeholder'];
+			
+		if (array_key_exists('onChange', $args) && strlen($args['onChange']))
+			$this->onChange = $args['onChange'];
+			
+		if (array_key_exists('onKeydown', $args) && strlen($args['onKeydown']))
+			$this->onKeydown = $args['onKeydown'];
+			
+		if (array_key_exists('onFocus', $args) && strlen($args['onFocus']))
+			$this->onFocus = $args['onFocus'];
+			
+		if (array_key_exists('onBlur', $args) && strlen($args['onBlur']))
+			$this->onBlur = $args['onBlur'];
+			
 	}
 
 	function renderElement() {
-		return(sf('<input type="%s" name="%s" value="%s" id="form_%s" class="%s"%s />',
+		return(sf('<input type="%s" name="%s" value="%s" id="form_%s" class="%s"%s%s%s%s%s />',
 							$this->htmlType,
 							$this->getName(),
 							parent::makeInputSafe($this->getValue()),
 							$this->getName(),
 							$this->cssClassName,
-							strlen($this->placeholder)?sf(' placeholder="%s"', $this->placeholder):''
+							strlen($this->placeholder)?sf(' placeholder="%s"', $this->placeholder):'',
+							!is_null($this->onChange) && strlen($this->onChange)?sf(' onChange="%s"', $this->onChange):'',
+							!is_null($this->onKeydown) && strlen($this->onKeydown)?sf(' onKeydown="%s"', $this->onKeydown):'',
+							!is_null($this->onFocus) && strlen($this->onFocus)?sf(' onFocus="%s"', $this->onFocus):'',
+							!is_null($this->onBlur) && strlen($this->onBlur)?sf(' onBlur="%s"', $this->onBlur):''
 				));
 	}
 }
