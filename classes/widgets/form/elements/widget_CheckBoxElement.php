467f6ac39a666d023eacfa203290f627960384b5
Jimmy Forrester-Fellowes <jimmyxx@gmail.com>
2011-05-11 14:51:50 +0100

    Added onClick event hook-in to checkbox element

--- classes/widgets/form/elements/widget_CheckBoxElement.php
+++ classes/widgets/form/elements/widget_CheckBoxElement.php
@@ -2,9 +2,13 @@
 
 class widget_CheckBoxElement extends widget_AbstractElement {
 
+	protected $onClick;
 
 	public function __construct($args) {
 
+		if (array_key_exists('onClick', $args) && strlen($args['onClick']))
+			$this->onClick = $args['onClick'];
+			
 	}
 
 	public function getValue() {
@@ -15,10 +19,11 @@ class widget_CheckBoxElement extends widget_AbstractElement {
 		$this->value =(isset($input[$this->name]) && $input[$this->name] == 'on') ? true : false;
 	}
 	function renderElement() {
-		return(sf("<input type='checkbox' name='%s' %s id='form_%s' class='inputCheckbox' />",
+		return(sf("<input type='checkbox' name='%s' %s id='form_%s' class='inputCheckbox' %s />",
 							$this->getName(),
 							$this->getValue() ? "checked='checked'" : "",
-							$this->getName()
+							$this->getName(),
+							!is_null($this->onClick) && strlen($this->onClick)?sf(' onClick="%s"', $this->onClick):''
 				));
 	}
 }
