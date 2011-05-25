62c843f37701a6f4525b52103bcc5f847165fb52
Jimmy Forrester-Fellowes <jimmyxx@gmail.com>
2011-05-25 14:53:39 +0100

    added getResultsPerPage method to paginate. Didn't want to use limit incase we require that later for something else.

--- classes/widgets/pagination/widget_Paginate.php
+++ classes/widgets/pagination/widget_Paginate.php
@@ -101,6 +101,12 @@ class widget_Paginate {
 
 		return 	$this->resultsPerPage;
 	}
+	
+	
+	public function getResultsPerPage() {
+		return 	$this->resultsPerPage;
+	}
+	
 
 	/* set the format template (default is classic) */
 	public function setFormatTemplate ($template = 1) {
