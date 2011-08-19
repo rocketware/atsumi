614bafb8815306278123776cf96d452c61d928bf
Jimmy Forrester-Fellowes <jimmyxx@gmail.com>
2011-08-19 11:24:29 +0100

    Added content-length header to the text view

--- classes/mvc/views/mvc_TxtView.php
+++ classes/mvc/views/mvc_TxtView.php
@@ -1,13 +1,10 @@
 <?php
 /*
  * Created on 14 Mar 2008
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
+ * 
  */
 
 class mvc_TxtView extends mvc_AbstractView {
-
 
 	public function render() {
 		pfl('%s', $this->get_txt);
@@ -16,11 +13,9 @@ class mvc_TxtView extends mvc_AbstractView {
 	// headers
 	public function setHeaders() {
 		header(sf('Content-Type: text/plain; charset=%s', $this->getCharset()));
+		header(sf('Content-Length: %s', strlen($this->get_txt)));
 	}	
-	
-
-
-	
+		
 }
 
 ?>
