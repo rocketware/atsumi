dd0b106b93ee56a1b86a2fa451df14a6a0e06a9a
Jimmy Forrester-Fellowes <jimmy@rocketware.co.uk>
2014-05-06 16:44:56 +0000

    added a disabled state

--- classes/widgets/form/elements/widget_TextElement.php
+++ classes/widgets/form/elements/widget_TextElement.php
@@ -9,6 +9,7 @@ class widget_TextElement extends widget_AbstractElement {
 	protected $onKeydown		= null;
 	protected $onFocus			= null;
 	protected $onBlur			= null;
+	protected $disabled			= false;
 	protected $rows;
 
 	public function __construct($args) {
@@ -26,14 +27,17 @@ class widget_TextElement extends widget_AbstractElement {
 			
 		if (array_key_exists('onFocus', $args) && strlen($args['onFocus']))
 			$this->onFocus = $args['onFocus'];
-			
+
 		if (array_key_exists('onBlur', $args) && strlen($args['onBlur']))
 			$this->onBlur = $args['onBlur'];
+
+		if (array_key_exists('disabled', $args) && is_bool($args['disabled']))
+			$this->disabled = $args['disabled'];
 			
 	}
 
 	function renderElement() {
-		return(sf('<input type="%s" name="%s" value="%s" %s id="form_%s" class="%s" %s%s%s%s%s />',
+		return(sf('<input type="%s" name="%s" value="%s" %s id="form_%s" class="%s" %s%s%s%s%s%s />',
 			$this->htmlType,
 			$this->getName(),
 			parent::makeInputSafe($this->getValue()),
@@ -44,7 +48,8 @@ class widget_TextElement extends widget_AbstractElement {
 			!is_null($this->onChange) && strlen($this->onChange)?sf(' onChange="%s"', $this->onChange):'',
 			!is_null($this->onKeydown) && strlen($this->onKeydown)?sf(' onKeydown="%s"', $this->onKeydown):'',
 			!is_null($this->onFocus) && strlen($this->onFocus)?sf(' onFocus="%s"', $this->onFocus):'',
-			!is_null($this->onBlur) && strlen($this->onBlur)?sf(' onBlur="%s"', $this->onBlur):''
+			!is_null($this->onBlur) && strlen($this->onBlur)?sf(' onBlur="%s"', $this->onBlur):'',
+			$this->disabled?' disabled':''
 		));
 	}
 
