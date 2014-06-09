5002287c29b3af7ea8fbecb8e0f1a9dbf8ba9f77
Jimmy Forrester-Fellowes <jimmy@rocketware.co.uk>
2014-06-09 05:40:07 -0700

    added a http accepts language parser to locale model

--- classes/mvc/models/core/mvc_LocaleModel.php
+++ classes/mvc/models/core/mvc_LocaleModel.php
@@ -34,6 +34,33 @@ class mvc_LocaleModel extends mvc_AbstractModel {
 		);
 	}
 
+	static function parseHttpAcceptLanguage ($httpAcceptLanguage) {
+
+		$langs = array();
+
+		// break up string into pieces (languages and q factors)
+		preg_match_all('/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i',
+			$httpAcceptLanguage,
+			$langMatches
+		);
+
+		if (count($langMatches[1])) {
+			// create a list like "en" => 0.8
+			$langs = array_combine($langMatches[1], $langMatches[4]);
+
+			// set default to 1 for any without q factor
+			foreach ($langs as $lang => $val) {
+				if ($val === '') $langs[$lang] = 1;
+			}
+
+			// sort list based on value
+			arsort($langs, SORT_NUMERIC);
+			$langs = array_change_key_case($langs, CASE_LOWER);
+		}
+
+		return $langs;
+	}
+
 }
 
 ?>
