7ac808df383d017f559fa71025eb89ab26b064fb
Jimmy Forrester-Fellowes <jimmy@rocketware.co.uk>
2015-06-17 17:18:36 +0000

    https version of recaptcha

--- classes/widgets/form/elements/widget_RecaptchaElement.php
+++ classes/widgets/form/elements/widget_RecaptchaElement.php
@@ -12,9 +12,9 @@ class widget_RecaptchaElement extends widget_AbstractElement {
 	}
 	function renderElement() {
 		$output = sf('
-			<script type="text/javascript"src="http://www.google.com/recaptcha/api/challenge?k=%s"></script>
+			<script type="text/javascript"src="https://www.google.com/recaptcha/api/challenge?k=%s"></script>
 			<noscript>
-				<iframe src="http://www.google.com/recaptcha/api/noscript?k=%s" height="300" width="500" frameborder="0"></iframe><br>
+				<iframe src="https://www.google.com/recaptcha/api/noscript?k=%s" height="300" width="500" frameborder="0"></iframe><br>
 					<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
 					<input type="hidden" name="recaptcha_response_field" value="manual_challenge">
 			</noscript>', $this->publicKey, $this->publicKey);
